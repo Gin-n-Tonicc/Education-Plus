9d3d35c9e96db714d5b689d373770621946273be
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2023-02-03 22:48:46 +0200

    create template

--- server/src/main/java/com/example/demo/UserController.java
+++ server/src/main/java/com/example/demo/UserController.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
