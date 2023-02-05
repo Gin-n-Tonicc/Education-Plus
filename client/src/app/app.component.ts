36b3172a9212fd9917945127cf59a17708e7078c
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2023-02-05 11:58:53 +0200

    Added dynamic page titles

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivationStart, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'client';
+    title = 'client';
+
+    constructor(private router: Router, private titleService: Title) {
+        this.router.events
+            .pipe(
+                filter(
+                    (e): e is ActivationStart => e instanceof ActivationStart
+                ),
+                map((e) => e.snapshot.data?.['title']),
+                filter((d) => !!d)
+            )
+            .subscribe((pageTitle) => {
+                this.titleService.setTitle(pageTitle);
+            });
+    }
 }
